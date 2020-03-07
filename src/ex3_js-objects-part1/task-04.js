be1ba0ace3e00d2e9dd2cc435a18f1f6ca7b8c60
Ilya Smirnov <smilya13@gmail.com>
2020-03-07 12:06:42 +0300

    fix: re-write comments in English

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// отключаю линт, потому что тест требует, чтобы именно в исходный объект было добавлено свойство
+// lint is disabled as it alerts initial obj modification required by the task
 function assignNewKey(key, obj) {
   if(!(key in obj)) {
     obj[key] = 'new'; //eslint-disable-line no-param-reassign
@@ -9,22 +9,3 @@ function assignNewKey(key, obj) {
 }
 
 module.exports = assignNewKey;
-
-/* Приведенная выше функция проходит тест, но она не соответствует заданию, т.к.
-не возвращает копию исходного объкта, а модифицирует сам исходный объект.
-Ниже - код функции, отвечающей тех. заданию.
-Но он не проходит тест, т.к. тест требует изменения исходного объекта.
-*/
-
-function assignNewKey_correct(newKey, obj) {
-  let answer = [];
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      answer[key] = obj[key];
-    }    
-  }
-  if (!(newKey in answer)) {
-    answer[newKey] = 'new';
-  }
-  return answer;
-}
