44f7fc8c90490624e06f63abd97fd8b794f0212a
Ilya Smirnov <smilya13@gmail.com>
2020-05-13 17:50:09 +0300

    feat: inline images into webpack bundle

--- src/kanban/index.js
+++ src/kanban/index.js
@@ -1,5 +1,12 @@
 import './src/style.css';
 import './src/js/onload.js';
-import './src/images/account.svg';
-import './src/images/header__newList.svg';
-import './src/images/menu__burger.svg';
+
+//Images
+import account_img from './src/images/account.svg';
+document.querySelector('.account__icon').src = account_img;
+import newList_img from './src/images/header__newList.svg';
+document.querySelector('.newListButton__img').src = newList_img;
+import menu_img from './src/images/menu__burger.svg';
+document.querySelector('.menu__img').src = menu_img;
+import favicon_ico from './src/images/favicon.ico';
+document.querySelector('link[rel="icon"]').href = favicon_ico;
